ff179562f5cf6353d2ccdbb426e29e9b3cd1e796
Jesper Brynildsen <jesper_bry@hotmail.com>
2019-03-16 12:05:19 +0100

    Remove unused code

--- server.js
+++ server.js
@@ -10,26 +10,6 @@ const timeslots = require("./routes/api/timeslots");
 
 const app = express();
 
-// Cross-origin resource sharing (CORS)
-// Allows restricted resources
-// app.all("/*", (req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Content-type,Accept,X-Access-Token,X-Key"
-//   );
-//   if (req.method == "OPTIONS") {
-//     res.status(200).end();
-//   } else {
-//     next();
-//   }
-// });
-
-// view engine setup
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "ejs");
-
 // Middleware
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
