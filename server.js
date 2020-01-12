67f8008c86b1128a6d2627c45e77f81dc193156a
Jesper Brynildsen <jesper_bry@hotmail.com>
2020-01-12 12:32:28 +0100

    Fix CORE error when fetch

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser"); // Let us use requests
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const appointments = require("./routes/api/appointments");
 const timeslots = require("./routes/api/timeslots");
@@ -13,6 +14,7 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 app.use(logger("dev"));
 // app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, "public")));
